5dacc8de6a9a2c6b0f50bf3e30a2cd8fa4c5c1ae
ironsoul0 <timka2609@gmail.com>
2020-07-17 13:04:17 +0600

    add eslint

--- src/components/App.js
+++ src/components/App.js
@@ -2,108 +2,116 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import Identicon from 'identicon.js';
 import './App.css';
-import SocialNetwork from '../abis/SocialNetwork.json'
-import Game from '../abis/Game.json'
-import Navbar from './Navbar'
-import Main from './Main'
+import SocialNetwork from '../abis/SocialNetwork.json';
+import Game from '../abis/Game.json';
+import Navbar from './Navbar';
+import Main from './Main';
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const { web3 } = window;
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
     // Network ID
-    const networkId = await web3.eth.net.getId()
-    const networkData = SocialNetwork.networks[networkId]
-    if(networkData) {
-      console.log("starting tot fetch");
-      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
-      this.setState({ socialNetwork })
-      console.log(socialNetwork)
-      const postCount = await socialNetwork.methods.postCount().call()
-      console.log("postCount ", postCount)
-      this.setState({ postCount })
+    const networkId = await web3.eth.net.getId();
+    const networkData = SocialNetwork.networks[networkId];
+    if (networkData) {
+      console.log('starting tot fetch');
+      const socialNetwork = web3.eth.Contract(
+        SocialNetwork.abi,
+        networkData.address
+      );
+      this.setState({ socialNetwork });
+      console.log(socialNetwork);
+      const postCount = await socialNetwork.methods.postCount().call();
+      console.log('postCount ', postCount);
+      this.setState({ postCount });
       // Load Posts
-      for (var i = 1; i <= postCount; i++) {
-        const post = await socialNetwork.methods.posts(i).call()
+      for (let i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call();
         this.setState({
           posts: [...this.state.posts, post]
-        })
+        });
       }
       // Sort posts. Show highest tipped posts first
       this.setState({
-        posts: this.state.posts.sort((a,b) => b.tipAmount - a.tipAmount )
-      })
-      this.setState({ loading: false})
+        posts: this.state.posts.sort((a, b) => b.tipAmount - a.tipAmount)
+      });
+      this.setState({ loading: false });
     } else {
-      window.alert('SocialNetwork contract not deployed to detected network.')
+      window.alert('SocialNetwork contract not deployed to detected network.');
     }
   }
 
   createPost(content) {
-    this.setState({ loading: true })
-    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account }, async (receipt, hash) => {
-      console.log(receipt, hash)
-      await this.loadBlockchainData()
-      this.setState({loading: false})
-
-    })
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods
+      .createPost(content)
+      .send({ from: this.state.account }, async (receipt, hash) => {
+        console.log(receipt, hash);
+        await this.loadBlockchainData();
+        this.setState({ loading: false });
+      });
   }
 
   tipPost(id, tipAmount) {
-    this.setState({ loading: true })
-    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount, gas: 100000 })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods
+      .tipPost(id)
+      .send({ from: this.state.account, value: tipAmount, gas: 100000 })
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false });
+      });
   }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       account: '',
       socialNetwork: null,
       postCount: 0,
       posts: [],
       loading: true
-    }
+    };
 
-    this.createPost = this.createPost.bind(this)
-    this.tipPost = this.tipPost.bind(this)
+    this.createPost = this.createPost.bind(this);
+    this.tipPost = this.tipPost.bind(this);
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-              posts={this.state.posts}
-              createPost={this.createPost}
-              tipPost={this.tipPost}
-            />
-        }
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Main
+            posts={this.state.posts}
+            createPost={this.createPost}
+            tipPost={this.tipPost}
+          />
+        )}
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,40 +2,53 @@ import React, { Component } from 'react';
 import Identicon from 'identicon.js';
 
 class Main extends Component {
-
   render() {
     return (
       <div className="container-fluid mt-5">
         <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+          <main
+            role="main"
+            className="col-lg-12 ml-auto mr-auto"
+            style={{ maxWidth: '500px' }}
+          >
             <div className="content mr-auto ml-auto">
               <p>&nbsp;</p>
-                <form onSubmit={(event) => {
-                  event.preventDefault()
-                  const content = this.postContent.value
-                  this.props.createPost(content)
-                }}>
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  const content = this.postContent.value;
+                  this.props.createPost(content);
+                }}
+              >
                 <div className="form-group mr-sm-2">
                   <input
                     id="postContent"
                     type="text"
-                    ref={(input) => { this.postContent = input }}
+                    ref={(input) => {
+                      this.postContent = input;
+                    }}
                     className="form-control"
                     placeholder="What's on your mind?"
-                    required />
+                    required
+                  />
                 </div>
-                <button type="submit" className="btn btn-primary btn-block">Share</button>
+                <button type="submit" className="btn btn-primary btn-block">
+                  Share
+                </button>
               </form>
               <p>&nbsp;</p>
-              { this.props.posts.map((post, key) => {
-                return(
-                  <div className="card mb-4" key={key} >
+              {this.props.posts.map((post, key) => {
+                return (
+                  <div className="card mb-4" key={key}>
                     <div className="card-header">
                       <img
-                        className='mr-2'
-                        width='30'
-                        height='30'
-                        src={`data:image/png;base64,${new Identicon(post.author, 30).toString()}`}
+                        className="mr-2"
+                        width="30"
+                        height="30"
+                        src={`data:image/png;base64,${new Identicon(
+                          post.author,
+                          30
+                        ).toString()}`}
                       />
                       <small className="text-muted">{post.author}</small>
                     </div>
@@ -45,15 +58,23 @@ class Main extends Component {
                       </li>
                       <li key={key} className="list-group-item py-2">
                         <small className="float-left mt-1 text-muted">
-                          TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')} ETH
+                          TIPS:{' '}
+                          {window.web3.utils.fromWei(
+                            post.tipAmount.toString(),
+                            'Ether'
+                          )}{' '}
+                          ETH
                         </small>
                         <button
                           className="btn btn-link btn-sm float-right pt-0"
                           name={post.id}
                           onClick={(event) => {
-                            let tipAmount = window.web3.utils.toWei('0.1', 'Ether')
-                            console.log(event.target.name, tipAmount)
-                            this.props.tipPost(event.target.name, tipAmount)
+                            const tipAmount = window.web3.utils.toWei(
+                              '0.1',
+                              'Ether'
+                            );
+                            console.log(event.target.name, tipAmount);
+                            this.props.tipPost(event.target.name, tipAmount);
                           }}
                         >
                           TIP 0.1 ETH
@@ -61,7 +82,7 @@ class Main extends Component {
                       </li>
                     </ul>
                   </div>
-                )
+                );
               })}
             </div>
           </main>
